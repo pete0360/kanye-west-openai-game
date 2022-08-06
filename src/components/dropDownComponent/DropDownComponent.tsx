d92b3c374ad5a7fff05f3fcbf9e0f13a40f6a968
bpeterkins28 <ben.peterkins@gmail.com>
2022-08-05 21:48:56 -0400

    changed dropdown fontsize

--- src/components/dropDownComponent/DropDownComponent.tsx
+++ src/components/dropDownComponent/DropDownComponent.tsx
@@ -12,14 +12,14 @@ export default function DropDownComponent() {
         className="w-screen h-screen absolute inset-0 bg-black z-40 opacity-[.7]"
       ></div>
       <div className="bg-dropDownCard absolute z-50 w-4/5 h-3/5 md:w-3/5 md:h-3/5  left-1/2 -translate-x-1/2 translate-y-1/2 -top-24 rounded-lg text-center flex flex-col justify-start p-2">
-        <div className=" font-sans text-4xl font-semibold my-12">
+        <div className=" font-sans text-4xl md:text-5xl font-semibold my-12">
           How to play
         </div>
-        <div className=" font-sans text-xl font-light">
+        <div className=" font-sans text-xl md:text-2xl font-light">
           You'll be given a quote. It is either made by Kanye West himself, or a
           bot that has been modeled to be like Kanye West.
         </div>
-        <div className=" font-sans text-xl font-light mt-4">
+        <div className=" font-sans text-xl md:text-2xl font-light mt-4">
           By guessing correctly, you get points. If you guess wrong, however,
           then it's game over.
         </div>
